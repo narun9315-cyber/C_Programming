2e3c9539fdedef4727625208b0afc2bef46ff059
narun9315-cyber <narun9315@gmail.com>
2025-11-02 22:20:29 +0530

    factorial in c

---
+++ arr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Function that calculates factorial iteratively using 3 parameters
+int factorial(int n, int fact, int i) {
+    fact = 1; // initialize factorial
+    for(i = 1; i <= n; i++) {
+        fact = fact * i;
+    }
+    return fact;
+}
+
+int main() {
+    int a, i;
+    int fact;
+
+    printf("Enter a number: ");
+    scanf("%d", &a);
+
+    fact = factorial(a, fact, i); // call the function
+
+    printf("Factorial: %d\n", fact);
+
+    return 0;
+}
